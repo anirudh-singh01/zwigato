8efc9bd73191bcd04d3024fbffdda8bcaa2b8096
anirudh-singh01 <anirudhsingh.ani321@gmail.com>
2025-12-11 22:51:16 +0530

    New values

---
+++ src/main/java/com/example/zwigato/dao/CustomerRepository.java
@@ -0,0 +1,6 @@
+package com.example.zwigato.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository {
+}
---
+++ src/main/java/com/example/zwigato/service/CustomerService.java
@@ -0,0 +1,4 @@
+package com.example.zwigato.service;
+
+public class CustomerService {
+}
